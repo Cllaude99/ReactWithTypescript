64c067aa79e50edf9f4b50dcf9919cba5738ca05
TaeYoon Kim <xodbsrla1025@naver.com>
2023-02-13 20:27:38 +0900

    Update

--- ch03/ch03_5/src/App.tsx
+++ ch03/ch03_5/src/App.tsx
@@ -1,22 +1,13 @@
-import DirectionTest from './pages/DirectionTest'
-import WrapTest from './pages/WrapTest'
-import MinMaxTest from './pages/MinMaxTest'
-import JustifyCenterTest from './pages/JustifyCenterTest'
-import AlignTest from './pages/AlignTest'
-import UserContainer from './pages/UserContainer'
-import CardContainer from './pages/CardContainer'
-
+import ButtonTest from './pages/ButtonTest'
+import InputTest from './pages/InputTest'
+import ModalTest from './pages/ModalTest'
 
 export default function App(){
   return(
     <main>
-      <CardContainer />
-      <UserContainer />
-      <AlignTest />
-      <JustifyCenterTest />
-      <MinMaxTest />
-      <WrapTest />
-      <DirectionTest />
+      <ButtonTest />
+      <InputTest />
+      <ModalTest />
     </main>
   )
 }
