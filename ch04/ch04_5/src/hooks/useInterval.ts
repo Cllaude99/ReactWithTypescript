f4dbed43a5bbc3928baab8560cd573ca4f1ba93c
TaeYoon Kim <xodbsrla1025@naver.com>
2023-02-16 15:52:04 +0900

    Update

---
+++ ch04/ch04_5/src/hooks/useInterval.ts
@@ -0,0 +1,8 @@
+import { useEffect, useReducer } from 'react';
+
+export const useInterval = (callback: () => void, duration: number = 1000) => {
+  useEffect(() => {
+    const id = setInterval(callback, duration)
+    return () => clearInterval(id)
+  }, [callback, duration])
+}
